089ab81903f33ce7027bb7fff3989efccc69f41c
Stepan-Kolarovsky <stepan.kolarovsky@student.ossp.cz>
2020-02-11 09:38:48 +0100

    přidano php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$name = "Štěpán Kolarovský"
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <h1><?php echo $name; ?></h1>
+ <li>Pohlaví: </li>
+   Muž <br />
+ <li>Základní škola</li>
+  Základní škola Velký Osek <br />
+ <li>Datum narození</li>
+  6.3.2004 <br />
+  
+  <li>E-mail</li> 
+   stepan.kolarovsky@student.ossp.cz <br />
+  <li>Telefon</li> 
+   +420 723 610 817<br />
+  <li>Koníčky</li>
+   Floorball <br />
+   Práce na počítači <br />
+</body>
+</html>
